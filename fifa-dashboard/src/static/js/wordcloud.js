879a090cee8c8e8ebc3255f2856016eebed4220e
tgurram9 <tejeshwarreddy1996@gmail.com>
2022-05-11 14:13:45 -0400

    Update word cloud

--- fifa-dashboard/src/static/js/wordcloud.js
+++ fifa-dashboard/src/static/js/wordcloud.js
@@ -1,14 +1,16 @@
 function wordCloud(data) {
   // List of words
-  var myWords = data.slice(0, 100);
-  console.log(data)
+  var myWords = data.slice(0, 10);
+  console.log(myWords)
 
-  // var myWords = data;
+  // myWords = [{word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"} ];
+
+  // console.log(myWords)
 
   // set the dimensions and margins of the graph
   var margin = { top: 10, right: 10, bottom: 10, left: 10 },
-    width = 450 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+    width = 400 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
   var word_svg = d3
@@ -26,7 +28,7 @@ function wordCloud(data) {
     .size([width, height])
     .words(
       myWords.map(function (d) {
-        // console.log(d)
+        console.log(d)
         return { text: d.name, size: d.count };
       })
     )
@@ -35,8 +37,8 @@ function wordCloud(data) {
       return ~~(Math.random() * 2) * 90;
     })
     .fontSize(function (d) {
-        // console.log(d)
-      return d.size+"";
+        console.log(d)
+      return d.size;
     }) // font size of words
     .on("end", draw);
   layout.start();
@@ -56,7 +58,7 @@ function wordCloud(data) {
       .append("text")
       .attr("font-size", function (d) {
         console.log(d);
-        return d.count;
+        return d.size;
       })
       .style("fill", "#69b3a2")
       .attr("text-anchor", "middle")
